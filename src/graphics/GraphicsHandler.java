23352fe2dad85b0db7c29e154d8e3af5c9c5d4b3
djhepker <djhepker@uiowa.edu>
2025-01-06 21:32:16 -0600

    Optimize GraphicsHandler variables, no longer updates every int every iteration, instead uses simple windowResized flag

--- src/graphics/GraphicsHandler.java
+++ src/graphics/GraphicsHandler.java
@@ -13,6 +13,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -22,14 +24,30 @@ public class GraphicsHandler extends JPanel {
     private GameWindow gameWindow;
     private InputHandler inputHandler;
 
+    private int entityWidth;
+    private int entityHeight;
+    private boolean windowResized;
+
+
     public GraphicsHandler(EntityList cells, EntityList pieces) {
         this.cells = cells;
         this.pieces = pieces;
         this.gameWindow = new GameWindow(this);
         this.inputHandler = new InputHandler(this);
+        this.entityWidth = 0;
+        this.entityHeight = 0;
+        this.windowResized = true;
 
         Border blackLine = BorderFactory.createLineBorder(Color.BLACK, 8); // 10px black border
         setBorder(blackLine);
+
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                windowResized = true;
+                super.componentResized(e);
+            }
+        });
 
         addMouseListener(new MouseAdapter() {
             @Override
@@ -44,6 +62,10 @@ public class GraphicsHandler extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
+        if (windowResized) {
+            updateEntitySize();
+        }
+
         drawBoard(g2d);
         drawPieces(g2d);
         if (inputHandler.getFirstXPos() != -1) {
@@ -53,39 +75,32 @@ public class GraphicsHandler extends JPanel {
         }
     }
 
+    private void updateEntitySize() {
+        entityWidth = getWidth() / 8;
+        entityHeight = getHeight() / 8;
+        windowResized = false;
+    }
+
     private void drawBoard(Graphics2D g2d) {
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
-
-        int cellWidth = panelWidth / 8;
-        int cellHeight = panelHeight / 8;
-
         for (Entity entity : cells) {
             Cell cell = (Cell) entity;
-            int xPos = cell.getX() * cellWidth;
-            int yPos = cell.getY() * cellHeight;
-            g2d.drawImage(cell.getSprite(), xPos, yPos, cellWidth, cellHeight, null); // Resize the cell
+            int xPos = cell.getX() * entityWidth;
+            int yPos = cell.getY() * entityHeight;
+            g2d.drawImage(cell.getSprite(), xPos, yPos, entityWidth, entityHeight, null); // Resize the cell
         }
     }
 
-    private void drawPieces(Graphics2D g2d) {
-        int panelWidth = getWidth();
-        int panelHeight = getHeight();
-
-        int pieceWidth = panelWidth / 8;
-        int pieceHeight = panelHeight / 8;
-
-        for (Entity entity : pieces) {
+    private void drawPieces(Graphics2D g2d) {for (Entity entity : pieces) {
             if (entity.getName().equals("DarkPiece")) {
                 DarkPiece dark = (DarkPiece) entity;
-                int xPos = dark.getX() * pieceWidth;
-                int yPos = dark.getY() * pieceHeight;
-                g2d.drawImage(dark.getSprite(), xPos, yPos, pieceWidth, pieceHeight, null);
+                int xPos = dark.getX() * entityWidth;
+                int yPos = dark.getY() * entityHeight;
+                g2d.drawImage(dark.getSprite(), xPos, yPos, entityWidth, entityHeight, null);
             } else if (entity.getName().equals("LightPiece")) {
                 LightPiece light = (LightPiece) entity;
-                int xPos = light.getX() * pieceWidth;
-                int yPos = light.getY() * pieceHeight;
-                g2d.drawImage(light.getSprite(), xPos, yPos, pieceWidth, pieceHeight, null);
+                int xPos = light.getX() * entityWidth;
+                int yPos = light.getY() * entityHeight;
+                g2d.drawImage(light.getSprite(), xPos, yPos, entityWidth, entityHeight, null);
             }
         }
     }
