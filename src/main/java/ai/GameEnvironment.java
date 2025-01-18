aac9dab6aacbf630f7d18bef20478f7b8f7c53f1
djhepker <djhepker@uiowa.edu>
2025-01-18 01:16:28 -0600

    organized comments, planned out ai algorithm

---
+++ src/main/java/ai/GameEnvironment.java
@@ -0,0 +1,9 @@
+package main.java.ai;
+
+public class GameEnvironment {
+    private int numStates;
+    private int numActions;
+    private int goalPointsState = 1600;
+
+
+}
---
+++ src/main/java/ai/agentutils/ActionNode.java
@@ -0,0 +1,17 @@
+package main.java.ai.agentutils;
+
+public class ActionNode {
+    private ActionNode next;
+    private double rInstant;
+
+    public ActionNode() {
+        /*
+        * TODO: Determine logic for creating an actionlist
+        *  probably need to make the LocationList organized in game updates
+        * */
+    }
+
+    public void addNode(ActionNode next) {
+
+    }
+}
