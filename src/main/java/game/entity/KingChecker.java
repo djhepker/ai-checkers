b91dae2a73ae7ced6aa13d1dd075816fcca0722e
djhepker <djhepker@uiowa.edu>
2025-01-25 14:54:26 -0600

    created todo for kingchecker class

--- src/main/java/game/entity/KingChecker.java
+++ src/main/java/game/entity/KingChecker.java
@@ -4,6 +4,10 @@ import main.java.game.utils.GameBoardPiece;
 
 import java.awt.image.BufferedImage;
 
+/*
+* TODO: create movement logic for KingChecker, likely just need to Override movePiece()
+* */
+
 public class KingChecker extends Checker implements GameBoardPiece {
     public KingChecker(String name, int x, int y, BufferedImage image) {
         super(name, x, y, image);
