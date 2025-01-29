7ea03107930e83b0ffe351561652f093a9e8e8de
dusti <djhepker@uiowa.edu>
2025-01-28 21:38:28 -0600

    reset kingmove

--- src/main/java/game/entity/piece/KingChecker.java
+++ src/main/java/game/entity/piece/KingChecker.java
@@ -23,64 +23,39 @@ public class KingChecker extends Checker {
      *   STATECODE:
      *   Stationary: 4
      *   Post-jump stationary: 3
-     *   Northwest: -1
-     *   Northeast: 1
-     *   Southwest: -2
-     *   SouthEast: 2
+     *   Northeast: -2
+     *   Southeast: -1
+     *   Northwest: 1
+     *   Southwest: 2
      * */
     @Override
     public void generateLegalMoves(PieceManager pMgr) {
         Deque<MoveState> taskQueue = new ArrayDeque<>();
         taskQueue.push(new MoveState(getX(), getY(), 4));
         while (!taskQueue.isEmpty()) {
-            System.out.println(++DEBUG_TIMER);
             MoveState currState = taskQueue.pop();
             int stateCode = currState.getStateCode();
-            int[] xDirectionArray;
-            int[] yDirectionArray = switch (stateCode) {
-                case -2 -> {
-                    xDirectionArray = new int[]{-1};
-                    yield new int[]{1};
-                }
-                case -1 -> {
-                    xDirectionArray = new int[]{-1};
-                    yield new int[]{-1};
-                }
-                case 1 -> {
-                    xDirectionArray = new int[]{1};
-                    yield new int[]{-1};
-                }
-                case 2 -> {
-                    xDirectionArray = new int[]{1};
-                    yield new int[]{1};
-                }
-                default -> {
-                    xDirectionArray = new int[]{-1, 1};
-                    yield new int[]{-1, 1};
-                }
-            };
-
-            for (int xDirection : xDirectionArray) {
-                int xNext = currState.getX() + xDirection;
+            int[] directionArr = getDirectionArr(stateCode);
+            for (int i = 0; i < directionArr.length; ++i) {
+                int xNext = directionArr[i++];
                 if (0 <= xNext && xNext < 8) {
-                    for (int yDirection : yDirectionArray) {
-                        int yNext = currState.getY() + yDirection;
-                        if (0 <= yNext && yNext < 8) {
-                            GameBoardPiece target = pMgr.getPiece(xNext, yNext);
-                            if (target == null) {   // target open case
-                                if (stateCode == 4) {   // target open; stationary
-                                    moveMgr.addLocationNode(getX(), getY(), xNext, yNext);
-                                } else if (stateCode < 3) { //  target open; mid-jump; direction acknowledged;
-                                    ActionNode nextSpace = getJumpLandingNode(currState, xNext, yNext, pMgr);
-                                    taskQueue.push(new MoveState(
-                                            xNext, yNext, 3, nextSpace.getCapturedNodes()));
-                                }
-                            } else if (stateCode > 2 && target.getColor() != super.getColor()) {  // target not open;
-                                if (stateCode == 3) { // post-jump; target !null; stationary;
-                                    taskQueue.push(new MoveState(xNext, yNext, xDirection, currState.getCapture()));
-                                } else {    // beginning position
-                                    taskQueue.push(new MoveState(xNext, yNext, xDirection));
-                                }
+                    int yNext = directionArr[i];
+                    if (0 <= yNext && yNext < 8) {
+                        GameBoardPiece target = pMgr.getPiece(xNext, yNext);
+                        if (target == null) { // cell open
+                            if (stateCode == 4) {
+                                moveMgr.addLocationNode(getX(), getY(), xNext, yNext);
+                            } else if (stateCode < 3) { // cell open; mid-jump
+                                ActionNode nextSpace = getJumpLandingNode(currState, xNext, yNext, pMgr);
+                                taskQueue.push(new MoveState(
+                                        xNext, yNext, 3, nextSpace.getCapturedNodes()));
+                            }
+                        } else if (stateCode > 2 && target.getColor() != super.getColor()) { // enemy occupied;
+                            if (stateCode == 3) { // post-jump; enemy located
+                                taskQueue.push(new MoveState(
+                                        xNext, yNext, getStateCode(xNext, yNext), currState.getCapture()));
+                            } else { // starting position; enemy located;
+                                taskQueue.push(new MoveState(xNext, yNext, getStateCode(xNext, yNext)));
                             }
                         }
                     }
@@ -88,6 +63,54 @@ public class KingChecker extends Checker {
             }
         }
         DEBUG_TIMER = 0;
+    }
+
+    /*
+     *   STATECODE:
+     *   Stationary: 4
+     *   Post-jump stationary: 3
+     *   Northeast: -2
+     *   Southeast: -1
+     *   Northwest: 1
+     *   Southwest: 2
+     * */
+    private int getStateCode(int xNext, int yNext) {
+        if (xNext == 1) {
+            if (yNext == 1) {
+                return -1;
+            }
+            return -2;
+        } else if (xNext == -1) {
+            if (yNext == 1) {
+                return 2;
+            }
+            return 1;
+        }
+        return 100000;
+    }
+
+    private int[] getDirectionArr(int stateCode) {
+        int[] directionArr = switch (stateCode) {
+            case -2 -> {
+                yield new int[]{1,-1};
+            }
+            case -1 -> {
+                yield new int[]{1,1};
+            }
+            case 1 -> {
+                yield new int[]{-1,-1};
+            }
+            case 2 -> {
+                yield new int[]{-1,1};
+            }
+            default -> {
+                yield new int[]{ 1,-1,
+                                 1,1,
+                                 -1,-1,
+                                 -1,1 };
+            }
+        };
+        return directionArr;
     }
 
     @Override
