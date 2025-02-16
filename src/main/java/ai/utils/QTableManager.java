b05aa35537cbd4a2ebd45d0fd0ac716a2006985a
dusti <djhepker@uiowa.edu>
2025-02-15 19:04:24 -0600

    final commit

--- src/main/java/ai/utils/QTableManager.java
+++ src/main/java/ai/utils/QTableManager.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class QTableManager {
     private HashMap<String, double[]> qTable;
     private SQLITEDatabase db;
