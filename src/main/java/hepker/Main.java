b80c408ab1851d37ac2af16eaadd74e39b068885
dusti <djhepker@uiowa.edu>
2025-02-25 11:06:45 -0600

    final push

--- src/main/java/hepker/Main.java
+++ src/main/java/hepker/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         boolean TRAINING_MODE = true;
         int epochs = 10;
-        int episode = 0;
+        int episodeCount = 0;
         GameLoop gameLoop;
 
         do {
             gameLoop = new GameLoop(new GameEngine(TRAINING_MODE));
             gameLoop.start();
             gameLoop.awaitCompletion();
-        } while (TRAINING_MODE && episode++ < epochs);
+        } while (TRAINING_MODE && episodeCount++ < epochs);
 
         System.out.println("Loop ended. Game finished");
     }
