a93063f7a1b7520aa2bb99a9ca88d9a03e5d1f3b
dusti <djhepker@uiowa.edu>
2025-03-06 10:26:16 -0600

    todo comments & logger info in gameloop

--- src/main/java/hepker/engine/GameLoop.java
+++ src/main/java/hepker/engine/GameLoop.java
@@ -1,8 +1,12 @@
 package hepker.engine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class GameLoop implements Runnable {
     private static final long FRAME_TIME = 8_333_333; // 10^9 / FRAME_TIME = Frames Per Second
     private static final long FRAME_DELAY_CONSTANT = 1_000_000;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameLoop.class);
 
     private final GameEngine game;
 
@@ -12,10 +16,12 @@ public final class GameLoop implements Runnable {
         this.game = inputEngine;
     }
 
-    /**
-     * TODO Make graphing calls outside of the loop thread; SwingUtilities cannot be assigned inside
-     * of the delegated game thread because they run asynchronously
-     */
+    /*
+    * TODO Make graphing calls outside of the loop thread; SwingUtilities cannot
+    *   be assigned inside of the delegated game thread because they run asynchronously
+    *   MAY need to use executor service
+    *   MAY put graphics on its own thread
+    * */
     @Override
     public void run() {
         while (!game.gameOver() && !Thread.currentThread().isInterrupted()) {
@@ -30,10 +36,10 @@ public final class GameLoop implements Runnable {
                             (int) (sleepTimeNanos % FRAME_DELAY_CONSTANT)
                     );
                 } catch (InterruptedException e) {
+                    LOGGER.error("Thread interrupted in GameLoop", e);
                     Thread.currentThread().interrupt();
                     return;
                 }
-
                 while (System.nanoTime() - startTime < FRAME_TIME) {
                     Thread.yield();
                 }
@@ -45,12 +51,14 @@ public final class GameLoop implements Runnable {
     public void start() {
         thread = new Thread(this);
         thread.start();
+        LOGGER.info("Game loop started");
     }
 
     public void awaitCompletion() {
         try {
             if (thread != null) {
                 thread.join();
+                LOGGER.info("Game loop awaiting completion");
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
