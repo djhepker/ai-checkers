cad7b5a0f4be5ad108ed9ecc87f1b2bb83e58f24
dusti <djhepker@uiowa.edu>
2025-03-15 21:00:33 -0500

    unused classes removed

--- src/main/java/hepker/ai/agentintegration/AIDecisionHandler.java
+++ src/main/java/hepker/ai/agentintegration/AIDecisionHandler.java
@@ -3,7 +3,7 @@ package hepker.ai.agentintegration;
 import hepker.game.entity.GameBoardPiece;
 import hepker.game.entity.movement.ActionNode;
 import hepker.game.gameworld.PieceManager;
-import hepker.utils.DecisionHandler;
+import hepker.utils.AIEnvironment;
 import lombok.Getter;
 
 import static hepker.game.entity.GameBoardPiece.PieceColor.DUSKY;
@@ -11,7 +11,7 @@ import static hepker.game.entity.GameBoardPiece.PieceColor.DUSKY;
 /**
  * AI Utility for calculations
  * */
-public final class AIDecisionHandler implements DecisionHandler {
+public final class AIDecisionHandler implements AIEnvironment {
     private final PieceManager pMgr;
 
     private int numOptionsNaught;
